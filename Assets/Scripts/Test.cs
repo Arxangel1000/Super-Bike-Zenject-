0c0697d3702ea57bbe8f2c9200fbd04038207c1b
DESKTOP-1SAP1JH\Александр <dwebm@mail.ru>
2020-10-12 00:50:22 +0300

    Test zenject bindings

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+public class Test
+{
+   private int aTest = 2;
+
+   public int ATest
+   {
+      get => aTest;
+      set => aTest = value;
+   }
+
+   public Test()
+   {
+      
+   }
+}
---
+++ Assets/Scripts/Test2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+public class Test2
+{
+    private Test test;
+    public Test2(Test _test)
+    {
+        test = _test;
+    }
+
+    public int CountA()
+    {
+         test.ATest = 5;
+         return test.ATest;
+    }
+}
